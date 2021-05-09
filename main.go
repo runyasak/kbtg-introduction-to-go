27305d10480d52a28b96a1a3ffc99f890cb4b60a
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-05-09 23:17:45 +0700

    implement printf

--- main.go
+++ main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello Gopher!!!")
+
+	fmt.Printf("hello %s!!!!", "Gopher")
+
+	fmt.Println("next line")
+
+	fmt.Printf("hello %d!!!!\n", 66)
+
+	fmt.Printf("hello %v!!!!\n", "Gopher")
+	fmt.Printf("hello %v!!!!\n", 77)
+
+	fmt.Printf("hello %v : %v !! \n", "Gopher", 66)
 }
