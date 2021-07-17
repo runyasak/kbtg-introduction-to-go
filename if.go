803f353a02b403c9f6669b8b6516f90df276d63f
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-07-17 20:22:49 +0700

    add if and switch

---
+++ if.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 15
+
+	if num%2 == 0 {
+		fmt.Println("เลขคู่")
+	} else if num == 31 {
+		fmt.Println("คนมีคู่ไม่รู้หรอก")
+	} else {
+		fmt.Println("เลขโสด")
+	}
+}
